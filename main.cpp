a05aabc89cc5f9a0eb42c8cc3877ffc14b8d47bc
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-11 21:59:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,83 @@
-//including header files
 #include <iostream>
-
-//using namespace std for simplifying io
 using namespace std;
+int userOption;
+void printMenu()
+{
+    cout << "1: Print help" << endl;
+    cout << "2: Print exchange stats" << endl;
+    cout << "3: Place an ask" << endl;
+    cout << "4: Place a bid" << endl;
+    cout << "5: Print wallet" << endl;
+    cout << "6: Continue" << endl;
+}
 
-//main function
-int main() {
-    //main menu
-    cout<<"---------Current Exchange System-----------"<<endl;
-    cout<<"1: Print help"<<endl;
-    cout<<"2: Print exchange stats"<<endl;
-    cout<<"3: Make an offer"<<endl;
-    cout<<"4: Make a bid"<<endl;
-    cout<<"5: Print wallet"<<endl;
-    cout<<"6: Continue"<<endl;
-    cout<<"==============="<<endl;
-    //asking user for input
-    cout<<"Enter your choice: "<<endl;
+int getUserOption(){
+    cout << "Type in 1-6" << endl;
+    cin >> userOption;
+    return userOption;
+}
 
-    //declaring user option
-    int userOption;
-    //accepting input
-    cin>>userOption;
-    //showing what they chose
-    cout<<"You chose: "<<userOption<<endl;
+void printHelp()
+{
+    cout << "Help: Your aim is to make money, analyse the market and make bids and offers" << endl;
+}
+void printMarketStats()
+{
+    cout << "Market looks good" << endl;
+}
+void enterAsk(){
+    cout << "Make an offer - enter the amount" << endl;
+}
 
-    //handling the user choice
-    if(userOption==0){
-        cout<<"Wrong choice, choose 1-6"<<endl;
-    }
-    else if(userOption==1){
-        cout<<"Help: Your aim is to make money, analyse the market and make bids and offers"<<endl;
-    }
-    else if(userOption==2){
-        cout<<"Market looks good"<<endl;
-    }
-    else if(userOption==3){
-        cout<<"Make an offer - enter the amount"<<endl;
-    }
-    else if(userOption==4){
-        cout<<"Make a bid - enter the amount"<<endl;
-    }
-    else if(userOption==5){
-        cout<<"Your wallet is empty"<<endl;
-    }
-    else if(userOption==6){
-        cout<<"Going to next time frame"<<endl;
+void enterBid(){
+    cout << "Make a bid - enter the amount" << endl;
+}
 
+void printWallet(){
+    cout << "Your wallet is empty" << endl;
+}
+
+void gotoNextTimeframe(){
+    cout << "Going to next time frame" << endl;
+}
+void processUserOption(int userOption){
+    if (userOption == 0)
+    {
+        cout << "Wrong choice, choose 1-6" << endl;
+    }
+    else if (userOption == 1)
+    {
+        printHelp();
+    }
+    else if (userOption == 2)
+    {
+        printMarketStats();
+    }
+    else if (userOption == 3)
+    {
+        enterAsk();
+    }
+    else if (userOption == 4)
+    {
+        enterBid();
+    }
+    else if (userOption == 5)
+    {
+        printWallet();
+    }
+    else if (userOption == 6)
+    {
+        gotoNextTimeframe();
+    }
+}
+
+int main()
+{
+    while(true){
+        printMenu();
+
+        userOption = getUserOption();
+
+        processUserOption(userOption);
     }
 }
